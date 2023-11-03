c1ca62aeca444d75fd0127feb10f178caa08c9fe
isalaz <isalaz973@gmail.com>
2023-11-03 11:10:23 +0100

    removed out and added gitignore to folder

--- chalmers_al_am/src/process_P06.py
+++ chalmers_al_am/src/process_P06.py
@@ -15,6 +15,55 @@ import argparse
 import glob
 import os
 from typing import Set
+import numpy as np
+import datetime
+import h5py
+import natsort
+
+
+class Scan:
+    def __init__(self, root_path, sample_name, scan_number):
+        self.root_path = root_path
+        self.sample_name = sample_name
+        self.scan_number = scan_number
+        self.scan_str = 'scan_' + str(scan_number).zfill(5)
+        self.meta_data_path = os.path.join(root_path,'raw', sample_name, self.scan_str + '.nxs')
+        
+    def absolute_times(self):
+        
+        with h5py.File(self.meta_data_path, 'r') as f:
+            start_time = f['scan/start_time'][()].decode('utf-8')
+            end_time = f['scan/end_time'][()].decode('utf-8')
+            format_str = "%Y-%m-%dT%H:%M:%S.%f%z"
+            # Parse date string into datetime object
+            start_time = datetime.datetime.strptime(start_time, format_str)
+            end_time = datetime.datetime.strptime(end_time, format_str)
+        
+        time_path = os.path.join(self.root_path,'raw', self.sample_name, self.scan_str, "scantime_01" )
+        time_files = natsort.natsorted(glob.glob(os.path.join(time_path, "*.nxs" )))
+        dtimes = []
+        for f in time_files:
+            with h5py.File(f, 'r') as f_:
+                
+                dtime = f_['entry/data/deltatriggertime'][()]
+                dtimes.extend(list(dtime))
+        dtimes = np.array(dtimes)
+        cumtimes = np.cumsum(dtimes, dtype=np.float32)
+        times_seconds = np.round(cumtimes/1e6).astype(int) #dtimes are in µs
+        absolute_times = times_seconds + start_time.timestamp()
+        print(f'Time precision in s: {end_time.timestamp()-absolute_times[-1]}')
+        
+        self.absolute_times = absolute_times
+        
+    def xrf_data(self):
+        fluo_data_dir_path1 = os.path.join(self.root_path,'raw', self.sample_name, self.scan_str, "xspress3_mini_129" )
+        fluo_data_dir_path2 = os.path.join(self.data_path,'raw', self.sample_name, self.scan_str, "xspress3_mini_130" )
+    
+    
+    
+    
+        
+        
 
 def find_unique_sample_names(base_directory: str) -> Set[str]:
     """
@@ -45,6 +94,39 @@ def find_unique_sample_names(base_directory: str) -> Set[str]:
         unique_sample_names.add(sample_name)
     
     return unique_sample_names
+
+def absolute_times(root_path: str, sample_name:str, scan_number: int) -> np.array:
+    scan_str = str(scan_number) 
+    scan_str = 'scan_' + scan_str.zfill(5)
+    meta_data_path = os.path.join(root_path,'raw', sample_name, scan_str + '.nxs')
+    with h5py.File(meta_data_path, 'r') as f:
+        start_time = f['scan/start_time'][()].decode('utf-8')
+        end_time = f['scan/end_time'][()].decode('utf-8')
+        format_str = "%Y-%m-%dT%H:%M:%S.%f%z"
+        # Parse date string into datetime object
+        start_time = datetime.datetime.strptime(start_time, format_str)
+        end_time = datetime.datetime.strptime(end_time, format_str)
+    
+    time_path = os.path.join(root_path,'raw', sample_name, scan_str, "scantime_01" )
+    time_files = natsort.natsorted(glob.glob(os.path.join(time_path, "*.nxs" )))
+    dtimes = []
+    for f in time_files:
+        with h5py.File(f, 'r') as f_:
+            
+            dtime = f_['entry/data/deltatriggertime'][()]
+            dtimes.extend(list(dtime))
+    dtimes = np.array(dtimes)
+    cumtimes = np.cumsum(dtimes, dtype=np.float32)
+    times_seconds = np.round(cumtimes/1e6).astype(int) #dtimes are in µs
+    absolute_times = times_seconds + start_time.timestamp()
+    print(f'Time precision in s: {end_time.timestamp()-absolute_times[-1]}')
+    
+    return absolute_times
+    
+    
+    
+
+    
 
 def build_xrf_dataset(root_path: str, verbose: bool=False, config_file: str=None) -> None:
     """
